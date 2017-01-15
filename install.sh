198d53edde923ca04575a940ac3c7d0859ee364a
Harrison South <hsouth95@hotmail.co.uk>
2017-01-15 17:44:24 +0000

    Updated the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Running installer"
-
-sudo apt-get install sqlite3
-
+sudo echo "FLASK_APP=rasp_server" >> /etc/environment
+sudo pip install flask --upgrade
+sudo flask run -h 0.0.0.0
 echo "Finished installing"
